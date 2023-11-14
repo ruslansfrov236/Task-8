100f5cdef9c0339b068ffbbdeb7ec4e67da39b20
Ruslan Seferov <91841957+ruslansfrov236@users.noreply.github.com>
2023-11-14 13:43:49 +0400

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] arr = new int[0];
+arr = arr.Append(1).ToArray();
+arr = arr.Append(2).ToArray();
+arr = arr.Append(3).ToArray();
+arr = arr.Append(4).ToArray();
+
+int sum = 0;
+foreach (var item in arr)
+{
+    sum += item;
+}
+
+Console.WriteLine(sum);
